f73572cdb9dd84ba0108b56a627d0590c086c562
Vladimir Valchanov <v.valchanov@gmail.com>
2023-06-20 12:37:46 +0300

    feat #9 issue passwords

--- util/Constants.java
+++ util/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final String URL = "jdbc:mysql://localhost/data_base";
     public static final String URL_TEST = "jdbc:mysql://localhost/test_data_base";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "L31mz40123!";
+    public static final String PASSWORD = "8888888888888";
     public static final String ISBN_REGEX = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$";
     public static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
     public static final Pattern ISBN_PATTERN = Pattern.compile(ISBN_REGEX);
